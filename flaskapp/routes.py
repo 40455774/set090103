2f2b1f0b7ac33aec42a5c8f446952e28590925ca
40455774 <56398008+40455774@users.noreply.github.com>
2019-11-18 18:21:38 +0000

    Add files via upload

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from flaskapp import app, db, bcrypt
 from flaskapp.forms import RegistrationForm, LoginForm
 from flaskapp.models import User, Post
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 posts = [
     { 
@@ -57,7 +57,8 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             flash('Welcome to the site, {}', 'success')
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if  next_page else redirect(url_for('home'))
         else:
             flash('Login unsuccessful. Please check email and password', 'danger')
         
@@ -67,3 +68,8 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+    
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
