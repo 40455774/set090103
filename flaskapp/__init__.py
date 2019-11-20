c8ac5662ebcbf7e820b23878785646f75ab339ce
40455774 <56398008+40455774@users.noreply.github.com>
2019-11-20 17:47:31 +0000

    Update __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -15,7 +15,7 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'danger'
 
 app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
-app.config['MAIL_PORT'] = 25
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = '40455774@live.napier.ac.uk'
 app.config['MAIL_PASSWORD'] = 'kYcwee34!'
