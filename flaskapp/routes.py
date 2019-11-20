3869a3a8f1f893591c7a2682e73d56424fa297fe
40455774 <56398008+40455774@users.noreply.github.com>
2019-11-20 17:24:43 +0000

    Update routes.py

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskapp import app, db, bcrypt, mail
-from flask_mail import message
+from flask_mail import Message
 from flaskapp.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from flaskapp.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
